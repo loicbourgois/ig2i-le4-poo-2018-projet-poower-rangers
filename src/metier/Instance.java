3d2e2c60ec8a7abfbcc7492088e4f900a656da83
loicbourgois <loic@bourgois.pro>
2018-04-24 19:42:49 +0200

    Merge Instance

--- src/metier/Instance.java
+++ src/metier/Instance.java
@@ -5,7 +5,13 @@
  */
 package metier;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,9 +21,17 @@ public class Instance {
     private Entrepot entrepot;
     private Configuration config;
     private String pathToFile;
+		private int locationCount;
 
     public Instance() {
     }
+		
+		public Instance(String pathToFile) {
+			this.pathToFile = pathToFile;
+			this.locationCount = -1;
+			this.entrepot = new Entrepot();
+			this.config = null;
+		}
 
     public Instance(Entrepot entrepot, Configuration config, String pathToFile) {
         this.entrepot = entrepot;
@@ -69,7 +83,40 @@ public class Instance {
 
     @Override
     public String toString() {
-        return "Instance{" + "entrepot=" + entrepot.getId() + ", config=" + config + ", pathToFile=" + pathToFile + '}'+ "\n";
-    }
+			return "Instance{" 
+							+ "entrepot=" + entrepot.getId() 
+							+ ", config=" + config
+							+ ", pathToFile=" + pathToFile
+							+ ", locationCount=" + locationCount 
+							+ '}'+ "\n";
+		}
+		
+		private boolean parse() {
+		System.out.println("parsing");
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(this.pathToFile))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					if(line.startsWith("//NbLocations")) {
+						line = br.readLine();
+						this.locationCount = Integer.parseInt(line.replaceAll("\\s+",""));
+					}
+					// System.out.println(line);
+				}
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(Instance.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IOException ex) {
+			Logger.getLogger(Instance.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return true;
+	}
+	
+		public static void main(String[] args) {
+			System.out.println("Start Instance");
+			Instance instance = new Instance("./instances/instance_0116_131940_Z2.txt");
+			instance.parse();
+			System.out.println(instance.toString());
+		}
     
 }
