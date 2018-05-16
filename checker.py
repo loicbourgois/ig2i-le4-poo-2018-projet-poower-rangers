6c633ca8d81bd24237e41b23fc2f8d75762deccd
loicbourgois <loic@bourgois.pro>
2018-05-16 09:00:53 +0200

    Remove unecessary checker

--- checker.py
+++ checker.py
@@ -5,4 +5,4 @@ for root, dirs, files in os.walk('instances'):
   for instanceFile in files:
     solutionFile = instanceFile[:-4] + "_sol.txt"
     print(os.path.join("solutions", solutionFile))
-    call(["java", "-jar", "./CheckerBatchingPicking.jar", "-p", os.path.join("instances", instanceFile), os.path.join("solutions", solutionFile) ])
+    call(["java", "-jar", "./checker/CheckerBatchingPicking.jar", "-p", os.path.join("instances", instanceFile), os.path.join("solutions", solutionFile) ])
