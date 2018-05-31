64e86ee58961d89a854d1c3b05e75cbb1f79fd7a
loicbourgois <loic@bourgois.pro>
2018-05-31 11:20:29 +0200

    Lint Distance.java

--- src/metier/Distance.java
+++ src/metier/Distance.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package metier;
 
 import java.util.Objects;
@@ -17,75 +18,75 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
- *
+ * Distance from a localisation to another.
  * @author Rod
  */
 @Entity
-@Table(name="DISTANCE")
+@Table(name = "DISTANCE")
 public class Distance {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "ID")
-    private Integer id;
-    
-    @JoinColumn(name="ARRIVEE")
-    private Localisation arrivee;
-    
-    @Column(name="DISTANCE")
-    private Integer distance;
 
-    @JoinColumn(name = "LOCALISATION", referencedColumnName = "LOCALISATIONNO")
-    @ManyToOne(optional = false)
-    private Localisation localisation;
-    
-    public Distance() {
-    }
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "ID")
+	private Integer id;
 
-    public Distance(Localisation arrivee, Integer distance) {
-        this.arrivee = arrivee;
-        this.distance = distance;
-    }
+	@JoinColumn(name = "ARRIVEE")
+	private Localisation arrivee;
 
-    public Localisation getArrivee() {
-        return arrivee;
-    }
+	@Column(name = "DISTANCE")
+	private Integer distance;
 
-    public Integer getDistance() {
-        return distance;
-    }
+	@JoinColumn(name = "LOCALISATION", referencedColumnName = "LOCALISATIONNO")
+	@ManyToOne(optional = false)
+	private Localisation localisation;
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        return hash;
-    }
+	public Distance() {
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Distance other = (Distance) obj;
-        if (!Objects.equals(this.arrivee, other.arrivee)) {
-            return false;
-        }
-        if (!Objects.equals(this.distance, other.distance)) {
-            return false;
-        }
-        return true;
-    }
+	public Distance(Localisation arrivee, Integer distance) {
+		this.arrivee = arrivee;
+		this.distance = distance;
+	}
 
-    @Override
-    public String toString() {
-        return "Distance{" + "arrivee=" + arrivee + ", distance=" + distance + '}';
-    }
+	public Localisation getArrivee() {
+		return arrivee;
+	}
 
+	public Integer getDistance() {
+		return distance;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Distance other = (Distance) obj;
+		if (!Objects.equals(this.arrivee, other.arrivee)) {
+			return false;
+		}
+		if (!Objects.equals(this.distance, other.distance)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Distance{" + "arrivee=" + arrivee + ", distance=" + distance + '}';
+	}
 }
