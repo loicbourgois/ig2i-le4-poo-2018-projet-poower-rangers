8849cb9ab1da3631d78ea4a022e1c0292f60eaf0
loicbourgois <loic@bourgois.pro>
2018-05-31 12:18:51 +0200

    Lint QuantiteProduit.java

--- src/metier/QuantiteProduit.java
+++ src/metier/QuantiteProduit.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package metier;
 
 import java.util.Objects;
@@ -17,86 +18,83 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
- *
+ * Association d'un produit et de sa quantité.
  * @author Rod
  */
 @Entity
-@Table(name="QUANTITEPRODUIT")
+@Table(name = "QUANTITEPRODUIT")
 public class QuantiteProduit {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "ID")
-    private Integer id;
-    
-    @JoinColumn(name="PRODUIT")
-    private Produit produit;
-    
-    @Column(name="QUANTITE")
-    private Integer quantite;
-    
-    @JoinColumn(name = "COLIS", referencedColumnName = "COLISNO")
-    @ManyToOne(optional = false)
-    private Colis colis;
-    
-    @JoinColumn(name = "COMMANDE", referencedColumnName = "COMMANDENO")
-    @ManyToOne(optional = false)
-    private Commande commande;
-    
-    public QuantiteProduit() {
-    }
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "ID")
+	private Integer id;
 
-    public QuantiteProduit(Produit produit, Integer quantite) {
-        this.produit = produit;
-        this.quantite = quantite;
-    }
+	@JoinColumn(name = "PRODUIT")
+	private Produit produit;
 
-    public Produit getProduit() {
-        return produit;
-    }
+	@Column(name = "QUANTITE")
+	private Integer quantite;
 
-    public Integer getQuantite() {
-        return quantite;
-    }
+	@JoinColumn(name = "COLIS", referencedColumnName = "COLISNO")
+	@ManyToOne(optional = false)
+	private Colis colis;
 
-    public Integer getId() {
-        return id;
-    }
-    
-    
+	@JoinColumn(name = "COMMANDE", referencedColumnName = "COMMANDENO")
+	@ManyToOne(optional = false)
+	private Commande commande;
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        return hash;
-    }
+	public QuantiteProduit() {
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final QuantiteProduit other = (QuantiteProduit) obj;
-        if (!Objects.equals(this.produit, other.produit)) {
-            return false;
-        }
-        if (!Objects.equals(this.quantite, other.quantite)) {
-            return false;
-        }
-        return true;
-    }
+	public QuantiteProduit(Produit produit, Integer quantite) {
+		this.produit = produit;
+		this.quantite = quantite;
+	}
 
-    @Override
-    public String toString() {
-        return "QuantiteProduit{" + "produit=" + produit + ", quantite=" + quantite + '}';
-    }
-    
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public Integer getQuantite() {
+		return quantite;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final QuantiteProduit other = (QuantiteProduit) obj;
+		if (!Objects.equals(this.produit, other.produit)) {
+			return false;
+		}
+		if (!Objects.equals(this.quantite, other.quantite)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "QuantiteProduit{" + "produit=" + produit + ", quantite=" + quantite + '}';
+	}
 }
