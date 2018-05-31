0ff08f8e5e22bca7c83f448d5133f6435ce525ec
loicbourgois <loic@bourgois.pro>
2018-05-31 11:17:48 +0200

    Lint Produit.java

--- src/metier/Produit.java
+++ src/metier/Produit.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package metier;
 
 import javax.persistence.Column;
@@ -14,114 +15,123 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
- *
+ * Product. Has 2 dimensions : weight and volume. 
  * @author Rod
  */
 @Entity
-@Table(name="PRODUIT")
+@Table(name = "PRODUIT")
 public class Produit {
-    @Id
-    @Column(name="PRODUITNO")
-    private int id;
-    
-    @Column(name="POIDS")
-    private int poids;
-    
-    @Column(name="VOLUME")
-    private int volume;
-    
-    @JoinColumn(name="LOCALISATION", referencedColumnName ="LOCALISATIONNO")
-    private Localisation localisation;
-    
-    @JoinColumn(name = "ENTREPOT", referencedColumnName = "ENTREPOTNO")
-    @ManyToOne(optional = false)
-    private Entrepot entrepot;
-    
-    @JoinColumn(name = "QUANTITEPRODUIT", referencedColumnName = "ID")
-    @OneToOne
-    private QuantiteProduit quantiteProduit;
-    
-    @JoinColumn(name = "COLIS", referencedColumnName = "COLISNO")
-    @ManyToOne(optional = false)
-    private Colis colis;
-    
-    
-    public Produit() {
-    }
 
-    public Produit(int id, int poids, int volume, Localisation localisation) {
-        this.id = id;
-        if(poids < 0){
-            this.poids = 0;
-        }
-        else{
-            this.poids = poids;
-        }
-        if(volume < 0){
-            this.volume = 0;
-        }
-        else{
-            this.volume = volume;
-        }
-	if(localisation!=null){
-	    this.localisation=localisation;
+	@Id
+	@Column(name = "PRODUITNO")
+	private int id;
+
+	@Column(name = "POIDS")
+	private int poids;
+
+	@Column(name = "VOLUME")
+	private int volume;
+
+	@JoinColumn(name = "LOCALISATION", referencedColumnName = "LOCALISATIONNO")
+	private Localisation localisation;
+
+	@JoinColumn(name = "ENTREPOT", referencedColumnName = "ENTREPOTNO")
+	@ManyToOne(optional = false)
+	private Entrepot entrepot;
+
+	@JoinColumn(name = "QUANTITEPRODUIT", referencedColumnName = "ID")
+	@OneToOne
+	private QuantiteProduit quantiteProduit;
+
+	@JoinColumn(name = "COLIS", referencedColumnName = "COLISNO")
+	@ManyToOne(optional = false)
+	private Colis colis;
+
+	/**
+	 * Default constructor.
+	 */
+	public Produit() {
 	}
-	else{
-	    this.localisation = new Localisation();
+
+	/**
+	 * Real constructor.
+	 * @param id id
+	 * @param poids weight 
+	 * @param volume volume
+	 * @param localisation localisation
+	 */
+	public Produit(int id, int poids, int volume, Localisation localisation) {
+		this.id = id;
+		if (poids < 0) {
+			this.poids = 0;
+		} else {
+			this.poids = poids;
+		}
+		if (volume < 0) {
+			this.volume = 0;
+		} else {
+			this.volume = volume;
+		}
+		if (localisation != null) {
+			this.localisation = localisation;
+		} else {
+			this.localisation = new Localisation();
+		}
 	}
-    }
 
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        return hash;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		return hash;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Produit other = (Produit) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (this.poids != other.poids) {
-            return false;
-        }
-        if (this.volume != other.volume) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Produit other = (Produit) obj;
+		if (this.id != other.id) {
+			return false;
+		}
+		if (this.poids != other.poids) {
+			return false;
+		}
+		if (this.volume != other.volume) {
+			return false;
+		}
+		return true;
+	}
 
-    public int getId() {
-	return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getPoids() {
-	return poids;
-    }
+	public int getPoids() {
+		return poids;
+	}
 
-    public int getVolume() {
-	return volume;
-    }
+	public int getVolume() {
+		return volume;
+	}
 
-    public Localisation getLocalisation() {
-	return localisation;
-    }
+	public Localisation getLocalisation() {
+		return localisation;
+	}
 
-    @Override
-    public String toString() {
-        return "\t\t\tProduit{" + "id=" + id + ", poids=" + poids + ", volume=" + volume + "Localisation=" + localisation + '}'+ "\n";
-    }
-    
-    
-    
+	@Override
+	public String toString() {
+		return "\t\t\tProduit{" 
+						+ "id=" + id 
+						+ ", poids=" + poids 
+						+ ", volume=" + volume 
+						+ "Localisation=" + localisation 
+						+ '}' + "\n";
+	}
 }
